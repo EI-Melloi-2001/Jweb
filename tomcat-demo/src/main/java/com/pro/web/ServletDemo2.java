291c5cc5118222e958d9a33d170f4745bac57589
EI-Melloi-2001 <lvjiaxin18@outlook.com>
2022-02-22 13:31:22 +0800

    Initial commit

---
+++ tomcat-demo/src/main/java/com/pro/web/ServletDemo2.java
@@ -0,0 +1,66 @@
+package com.pro.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/demo2",loadOnStartup = 1)
+public class ServletDemo2 implements Servlet {
+
+    /**
+     * 初始化方法
+     * 1.调用时机：默认情况下Servlet被第一次访问时调用
+     * 2.调用次数：1
+     * 在Servlet被创建时执行，只执行一次。
+     * LoadOnStartUp:
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init...");
+
+
+    }
+
+
+    /**
+     * 获取ServletConfig对象
+     * @return
+     */
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     * 调用时机：每次Servlet被访问时调用
+     * 提供服务方法，每次Servlet被访问都会调用该方法
+     * @return
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet hello world~");
+    }
+
+
+    /**
+     * 获取Servlet信息
+     * @return
+     */
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+
+    /**
+     * 销毁
+     * 调用时机：内存释放或者服务器关闭时Servlet对象会被销毁
+     * 调用次数：1
+     */
+    @Override
+    public void destroy() {
+        System.out.println("destroy...");
+    }
+}
